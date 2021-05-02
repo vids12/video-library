38a671be6e7c67fc7d96b148379084f23a0ed25b
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-05-03 01:10:57 +0530

    refactored: searchBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
+import { useList } from "../DataProvider/VideoListProvider";
 import { SearchBar } from "./Searchbar";
 
 export function Navbar() {
+    const { setShowSearchPage,showSearchPage } = useList();
     return <div className="nav-bar">
       <img
           src="https://themeforshop.github.io/kala-pottery-demo/images/logo.png"
           alt="logo"
         />
         <nav>
-        <SearchBar />
+          <i className="fas fa-search" onClick={()=>setShowSearchPage(true)}></i>
+          {showSearchPage && <SearchBar />}
         </nav>
     </div>
   }
