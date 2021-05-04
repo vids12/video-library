b3447adff853ea75ebb7df16061897aa55e9e105
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-05-04 20:12:28 +0530

    styled: landingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,33 +1,29 @@
-import { useState } from "react";
-import { VideoPage } from "./videoPage";
 import { Link } from "react-router-dom";
 import { list } from "../DataProvider/data";
 import { useList } from "../DataProvider/VideoListProvider";
 
 export function LandingPage(){
-    const [ route,setRoute ] = useState(false);
     const { search } = useList();
     function getSearchData(list,search) {
         return list.filter(obj=>(obj.heading).toLowerCase().includes(search.toLowerCase()) ? obj : !obj)
     }
     const searchedData = getSearchData(list,search)
     return <div className="sidebar-page">
-        <ul className="video-column" style={{listStyle:"none"}}>
+        <ul className="video-column">
             {
                 searchedData.map(({id,avatarImg,heading,img}) =>{
                     return <li key={id} className="video-card">
-                        <img src={img} alt="Image" />
-                        <div className="video-heading">
-                            <img src={avatarImg} alt="avatar" className="avatar" />
-                            <span style={{fontSize:"0.6rem"}}>{heading}</span>
+                        <img src={`${img}`} alt="Image" />
+                        <div className="video-header">
+                            <img src={`${avatarImg}`} alt="avatar" className="avatar" />
+                            <span className="video-heading">{heading}</span>
                         </div>
                         <Link to={`/watch/${id}`} >
-                            <button onClick={()=>setRoute(true)}>Watch Now</button>
+                            <button className="primary-btn">Watch Now</button>
                         </Link>
                     </li>
                 })
             }
         </ul>
-        <div>{route && <VideoPage/>}</div>
     </div>
 }
